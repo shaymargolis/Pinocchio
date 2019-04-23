4eb00783f290db4496b332759f37247471ad6720
OfriZaks <zaksof@gmail.com>
2019-04-23 03:02:26 +0300

    Adding getHighestFreq function

--- audioTesting.py
+++ audioTesting.py
@@ -6,6 +6,24 @@ import subprocess
 import os
 from moviepy.video.io.VideoFileClip import VideoFileClip
 
+from scipy.interpolate import interp1d
+from scipy.signal      import argrelextrema
+
+def eac(sig, winsize=512, rate=44100):
+	"""Return the dominant frequency in a signal."""
+	s = np.reshape(sig[:len(sig)//winsize*winsize], (-1, winsize))
+	s = np.multiply(s, np.hanning(winsize))
+	f = np.fft.fft(s)
+	p = (f.real**2 + f.imag**2)**(1/3)
+	f = np.fft.rfft(p).real
+	q = f.sum(0)/s.shape[1]
+	q[q < 0] = 0
+	intpf = interp1d(np.arange(winsize//2), q[:winsize//2])
+	intp = intpf(np.linspace(0, winsize//2-1, winsize))
+	qs = q[:winsize//2] - intp[:winsize//2]
+	qs[qs < 0] = 0
+	return qs
+
 def runOnAllVideosFiles(persons, options, function, input = None):
     for person in persons:
         for option in options:
@@ -23,8 +41,8 @@ def cutVideo(tuple):
 
 def convertMov(tuple):
     filepath, extension = tuple
-    savepath = "Audio/" + filepath[7:-4] + extension
-    command = "ffmpeg -i "+filepath+" "+savepath
+    savepath = "Videos/" + filepath[7:-4] + extension
+    command = "ffmpeg -i "+filepath+" -vf scale=1080:720 "+savepath
     subprocess.call(command, shell=True)
 
 
@@ -41,13 +59,23 @@ def getFFT(filepath, s = 2048, savepath = ""):
             plt.figure(dpi = 250)
             plt.plot(freqs, 20 * np.log10(np.mean(pxx, axis=1)), color = 'darkviolet')
             plt.savefig(savepath)
-        return 20 * np.log10(np.mean(pxx, axis=1))
+        return np.fft.fftfreq(s, 1/rate), 20 * np.log10(np.mean(pxx, axis=1))
 
 
 #getFFT("Audio/Oded Kaplan/PT/PT-01.wav", savepath = "Audio/Oded Kaplan/PT/PT-01.eps")
 
+def getHighestFreq(filepath, save = False):
+    freq, y = getFFT(filepath)
+    freq = freq[:int(len(freq)/2)]
+    x = np.linspace(0, len(y)/rate, len(y))
+    if save:
+        plt.figure(dpi = 250)
+        plt.plot(freq, y[:-1], color='pink')
+        plt.savefig("Audio/Oded_Kaplan/PT/u2-FFT.eps")
+
+    return freq[y.argmax()]
 
 persons = ["Oded_Kaplan"]
 options = ["PT", "NT", "NL", "PL"]
-runOnAllVideosFiles(persons, options, convertMov, ".mp4")
+#runOnAllVideosFiles(persons, options, convertMov, ".avi")
 #cutVideo(("Videos/Oded_Kaplan/PT/PT-21.mov", 0.7))

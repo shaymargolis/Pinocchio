247be777fceb27fe10b1b30e6c8b297207c36cdc
Shay Maroglis <shay@inbar.co.il>
2019-04-29 03:13:42 +0300

    Fix net

--- net.py
+++ net.py
@@ -12,13 +12,13 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 class NetLearner:
-    def __init__(self, title):
+    def __init__(self, title, length):
         self.title = title
 
         self.model = keras.Sequential([
-            keras.layers.Dense(145, activation='relu'),
+            keras.layers.Dense(length, activation='relu'),
             keras.layers.Dropout(0.3, noise_shape=None, seed=None),
-            keras.layers.Dense(145, activation='relu'),
+            keras.layers.Dense(length, activation='relu'),
             keras.layers.Dropout(0.3, noise_shape=None, seed=None),
             keras.layers.Dense(1, activation='sigmoid')
         ])
@@ -39,21 +39,36 @@ class NetLearner:
         print('Test accuracy:', test_acc)
 
 if __name__ == "__main__":
-    learner = NetLearner("test1")
+    learner = NetLearner("test1", 145)
 
     truth = pd.read_csv("truth.csv")
     false = pd.read_csv("false.csv")
 
-    truth_y = np.ones(len(truth.iloc[:, 0]))
-    false_y = np.zeros(len(false.iloc[:, 0]))
+    truth = np.array(truth)
+    false = np.array(false)
 
-    X_train1, X_test1, Y_train1, Y_test1 = train_test_split(np.array(truth), np.array(truth_y))
-    X_train2, X_test2, Y_train2, Y_test2 = train_test_split(np.array(false), np.array(false_y))
+    odd = range(1, 144, 2)
+    even = range(0, 144, 2)
 
-    X_train = np.concatenate((X_train1, X_train2)).astype(np.float32)
-    Y_train = np.concatenate((Y_train1, Y_train2)).astype(np.float32)
-    X_test = np.concatenate((X_test1, X_test2)).astype(np.float32)
-    Y_test = np.concatenate((Y_test1, Y_test2)).astype(np.float32)
+    for i in range(len(truth)):
+        truth[i, even] -= truth[i, 16]
+        truth[i, odd] -= truth[i, 17]
+
+    for i in range(len(false)):
+        false[i, even] -= false[i, 16]
+        false[i, odd] -= false[i, 17]
+
+    print(truth[:, [16, 17]])
+
+    truth_y = np.ones(len(truth[:, 0]))
+    false_y = np.zeros(len(false[:, 0]))
+
+    data = np.concatenate((truth, false))
+    y = np.concatenate((truth_y, false_y))
+
+    print(y)
+
+    X_train, X_test, Y_train, Y_test = train_test_split(data, y)
 
     learner.learn(X_train, Y_train)
 
